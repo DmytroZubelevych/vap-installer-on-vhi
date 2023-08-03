dfbbe8e2a9770aa2e2be6dbe67bae95357ff7ce1
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2023-08-03 11:03:42 +0300

    Update vap.sh

--- installer/vap.sh
+++ installer/vap.sh
@@ -368,6 +368,7 @@ responseValidate(){
   errorsArray+="Name or service not known:API endpoint URL is invalid"
   errorsArray+="could not be found. (HTTP 400)"
   errorsArray+="ConflictException: 409: Client Error"
+  errorsArray+="already exists"
   
   while read -d, -r pair; do
     IFS=':' read -r key val <<<"$pair"
